6372f82667948980275173145640a24f84b5b8fa
User Hairy <hairy@eot-ukraine.su>
2014-04-16 20:26:57 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eotdigest::Application.config.secret_key_base = '65a5606fbdae227a340a2f652f473ca2cf15bb102b869af2c4c5fb3cdd2ce4746f9e196399889e1e9ea4f37e1482ce4d4927fde9636aedeaee81da7ee8c463a1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Eotdigest::Application.config.session_store :cookie_store, key: '_eotdigest_session'
