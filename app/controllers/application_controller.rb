5a6fa5759fa583ae090c2603684aca62d155fc53
Paul <tacid@tacid.kiev.ua>
2014-05-05 16:34:29 +0300

    made only auth access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  include Hobo::Controller::AuthenticationSupport
+  before_filter :except => [:login, :forgot_password, :accept_invitation, :do_accept_invitation, :reset_password,
+:do_reset_password] do
+    # Выставляем локаль согласно параметру locale
+    I18n.locale = params[:locale]
+    #p request
+    # отмечаем активность
+    logger.info("Session :user => #{session[:user]} #{current_user}");
+    #User.update(current_user.id, :last_active_at => DateTime.now.utc) if logged_in?
+
+    login_required unless User.count == 0
+  end
 end
