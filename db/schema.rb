83bea27dc41a84b522078aa1e6e761cf7fe223de
Alexandr Muratov <alexandr.muratov@eot.kiev.ua>
2014-05-14 23:13:48 +0300

    Gemfile.lock - deleted, route to front#index for new site, database.yml default

--- db/schema.rb
+++ db/schema.rb
@@ -30,8 +30,8 @@ ActiveRecord::Schema.define(version: 20140508135346) do
     t.integer  "poster_id"
   end
 
-  add_index "records", ["category_id"], name: "index_records_on_category_id"
-  add_index "records", ["poster_id"], name: "index_records_on_poster_id"
+  add_index "records", ["category_id"], name: "index_records_on_category_id", using: :btree
+  add_index "records", ["poster_id"], name: "index_records_on_poster_id", using: :btree
 
   create_table "sfilters", force: true do |t|
     t.string   "name"
@@ -55,6 +55,6 @@ ActiveRecord::Schema.define(version: 20140508135346) do
     t.string   "role",                                 default: "viewer"
   end
 
-  add_index "users", ["state"], name: "index_users_on_state"
+  add_index "users", ["state"], name: "index_users_on_state", using: :btree
 
 end
