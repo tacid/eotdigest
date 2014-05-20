e1a1dceb3ca5f474275fd4c3f0fb23cf761842e2
Paul <tacid@tacid.kiev.ua>
2014-05-21 00:56:16 +0300

    add DIGEST_REVISION constant

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+DIGEST_REVISION = `git shortlog -s | awk '{sum+=$1}  END {print sum}'`
+
 # Initialize the Rails application.
 Eotdigest::Application.initialize!
