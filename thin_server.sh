af7ad59f3f30a02c58b2c271943b69dc2fe3fc5b
Alexandr Muratov <alexandr.muratov@eot.kiev.ua>
2014-05-21 19:06:44 +0300

    thin server options changed

--- thin_server.sh
+++ thin_server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 BASEDIR=$(dirname $0)
 cd $BASEDIR
-/usr/local/bin/bundle exec thin -e production --tag EOTDIGEST --socket tmp/sockets/thin.sock $@
+/usr/local/bin/bundle exec thin -e production --tag EOTDIGEST -p 8080 $@
