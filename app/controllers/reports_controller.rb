43e277e19d9410b7470dba6ec68215607baed2e1
Paul <tacid@tacid.kiev.ua>
2014-05-29 21:10:26 +0300

    #1888 sort and search implemented for reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,4 +4,9 @@ class ReportsController < ApplicationController
 
   auto_actions :all, :except => [ :new ]
 
+  def index
+    reports = Report.search(params[:search], :name, :created_at, :content, :urlkey ).
+                      order_by(parse_sort_param(:name,:created_at,:urlkey))
+    hobo_index reports
+  end
 end
