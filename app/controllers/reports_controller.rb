d515492c3f0fced249ec479f88b5e5dd24ca0dd4
Paul <tacid@tacid.kiev.ua>
2014-05-30 03:39:57 +0300

    #1888 show public url when report is saved

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,6 +5,7 @@ class ReportsController < ApplicationController
   auto_actions :all, :except => [ :new ]
 
   def index
+    params[:sort] = '-created_at' if params[:sort].blank?
     reports = Report.search(params[:search], :name, :created_at, :content, :urlkey ).
                       order_by(parse_sort_param(:name,:created_at,:urlkey))
     hobo_index reports
