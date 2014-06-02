9c20e037baaf5595585e0f67563ea5f4b0e00d88
Paul <tacid@tacid.kiev.ua>
2014-06-02 20:19:44 +0300

    change app_name in config

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module Eotdigest
     config.assets.precompile += %w(admin.css admin.js)
     # Hobo: Named routes have changed in Hobo 2.0.   Set to false to emit both the 2.0 and 1.3 names.
     config.hobo.dont_emit_deprecated_routes = true
+    config.hobo.app_name = "Дайджест"
     # Hobo: the front subsite loads front.css & front.js
     config.assets.precompile += %w(front.css front.js)
     # Settings in config/environments/* take precedence over those specified here.
