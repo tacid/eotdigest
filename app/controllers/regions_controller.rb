fcae176b2a022a35ce279e1833f7cdf0f5e8caa9
Paul <tacid@tacid.kiev.ua>
2014-06-11 20:48:02 +0300

    regions sort and search implemented

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -4,4 +4,10 @@ class RegionsController < ApplicationController
 
   auto_actions :all, except: [ :new ]
 
+  def index
+    regions = Region.includes(:users).includes(:records).search(params[:search], :name).
+                        order_by(parse_sort_param(:name))
+    hobo_index regions
+  end
+
 end
