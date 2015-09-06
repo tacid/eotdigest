c8e27cc22101f435c29b1a246eee08de249d1841
Alexandr Muratov <alexandr.muratov@eot.kiev.ua>
2015-09-06 23:23:03 +0300

    fix: fix calculating treeorder of category #1973

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-DIGEST_REVISION = `git log | grep '^commit' | wc -l`
+DIGEST_REVISION = `/usr/local/bin/git log | grep '^commit' | wc -l`
 
 # Initialize the Rails application.
 Eotdigest::Application.initialize!
