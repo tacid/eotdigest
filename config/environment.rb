dd059102cadffb4ae7b64489a8437b479fbb97cc
Tacid <tacid@tacid.kiev.ua>
2018-01-29 17:24:42 +0300

    fix ENV check for EMAIL_FROM constant

--- config/environment.rb
+++ config/environment.rb
@@ -3,11 +3,11 @@ require File.expand_path('../application', __FILE__)
 
 DIGEST_REVISION = `/usr/bin/git log | grep '^commit' | wc -l`
 
-if RAILS_ENV('EMAIL_FROM').nil? then
+if ENV["DIGEST_EMAIL_FROM"].blank? then
     EMAIL = 'muv-digest@tacid.cf'
     EMAIL_FROM = '"MUV Digest" <'+EMAIL+'>' unless defined? EMAIL_FROM
 else
-    EMAIL_FROM = RAILS_ENV('EMAIL_FROM')
+    EMAIL_FROM = ENV["DIGEST_EMAIL_FROM"]
 end
 
 # Initialize the Rails application.
